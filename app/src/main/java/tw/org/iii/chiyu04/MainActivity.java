fc7c95483bd3b4ac7dab179fc9815166159a5f84
mmm <123>
2016-11-16 09:43:21 +0800

    Chiyu04 test

--- app/src/main/java/tw/org/iii/chiyu04/MainActivity.java
+++ app/src/main/java/tw/org/iii/chiyu04/MainActivity.java
@@ -1,7 +1,9 @@
 package tw.org.iii.chiyu04;
-
+// Activity 的生命週期
+// 以下七個方法 在管理生命週期 是自己會呼叫的 硬叫也沒用
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +12,39 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+    }
+
+    public void test1(View v){
+
+    }
+
 }
